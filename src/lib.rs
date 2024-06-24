2cf34fd7f065dc86bb713f3b39112070dbc6ec11
Not Applicable <ReactorScram@users.noreply.github.com>
2024-06-24 12:16:35 -0500

    make it more verbose

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use is_odd::IsOdd;
 
 pub trait IsEven {
-    fn is_even(&self) -> bool;
+    fn is_the_number_even(&self) -> bool;
 }
 
 macro_rules! prim_impl {
     ($($t:tt)*) => {
         $(
             impl IsEven for $t {
-                fn is_even(&self) -> bool {
+                fn is_the_number_even(&self) -> bool {
                     !self.is_odd()
                 }
             }
